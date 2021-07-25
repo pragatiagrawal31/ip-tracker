2cf986c3b55c60898df83cece39097367b25db80
Pragati Agrawal <pragati22066@gmail.com>
2021-07-25 23:18:17 +0530

    Minor upates to API calls

--- script/script.js
+++ script/script.js
@@ -28,36 +28,40 @@ updateMarker = (update_marker) => {
     L.marker(update_marker).addTo(map);
 }
 
-getIPDetails = (default_ip) => {
-    if(default_ip == undefined){
-        var ip_url = `${api_uri}${current_verion}?apiKey=${secret_api}`
-    }
-    else {
-        var ip_url = `${api_uri}${current_verion}?apiKey=${secret_api}&ipAddress=${default_ip}`
-    }
-    fetch(ip_url)
-    .then( results => results.json())
-    .then( data => {
+
+async function fetchDetails(default_ip)
+{
+    try{
+        if(default_ip == undefined){
+            var ip_url = `${api_uri}${current_verion}?apiKey=${secret_api}`
+        }
+        else {
+            var ip_url = `${api_uri}${current_verion}?apiKey=${secret_api}&ipAddress=${default_ip}`
+        }
+        const res = await fetch(ip_url);
+        const data = await res.json();
         current_ip.innerHTML = data.ip
         current_town.innerHTML = `${data.location.city} ${data.location.country} ${data.location.postalCode}`
-        current_zone.innerHTML = data.location.timezone
+        current_zone.innerHTML = `UTC ${data.location.timezone}`
         current_isp.innerHTML = data.isp
 
         // update map marker
         updateMarker([data.location.lat, data.location.lng])
-    })
-    .catch(error => {
+    }
+    catch(error){
+        fetchDetails();
         alert("Unable to get IP details")
         console.log(error)
-    })
+    }
 }
 
-document.addEventListener('load', getIPDetails())
+
+document.addEventListener('load', fetchDetails())
 
 search_btn.addEventListener('click', e => {
     e.preventDefault()
     if (entered_ip.value != '' && entered_ip.value != null) {
-        getIPDetails(entered_ip.value)
+        fetchDetails(entered_ip.value)
         return
     }
     alert("Please enter a valid IP address");
